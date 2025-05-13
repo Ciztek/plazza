5c2f33324168c1dedd7804ce04592e3821bc807f
Ciznia <gabriel.hosquet@epitech.eu>
2025-05-13 19:36:38 +0200

    Update to use parsed config value

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,4 +1,4 @@
-#include <stdexcept>
+#include "Config.hpp"
 
 #include "Kitchen.hpp"
 
@@ -6,33 +6,32 @@ using namespace Plazza;
 
 void Kitchen::cook(Pizza &pizza)
 {
-  auto recipe = _recipes.find(pizza.getType());
-  if (recipe == _recipes.end())
+  auto recipe = CONFIG_FILE.getRecipesByIds().find(pizza.getType());
+  if (recipe == CONFIG_FILE.getRecipesByIds().end())
     throw std::runtime_error("Unknown pizza type.");
-  auto ingredients = recipe->second;
-  for (auto ing: ingredients) {
-    if (ing == Fridge::Ingredient::COUNT)
-      break;
+  auto ingredients = recipe->second.second;
+  for (auto ing: ingredients)
     _fridge.consume(ing);
-  }
   pizza.setState(Pizza::State::COOKING);
   // TODO: Assign the pizza to a cook and simulate time
 }
 
 Kitchen::Fridge::Fridge()
 {
+  _stock = std::
+    vector<std::atomic<uint8_t>>(CONFIG_FILE.getIngredientsIds().size());
   for (auto &ingredient: _stock)
     ingredient.store(MAX_CAPACITY, std::memory_order_release);
 }
 
-auto Kitchen::Fridge::get(Ingredient ing) const -> uint8_t
+auto Kitchen::Fridge::get(size_t ing) const -> uint8_t
 {
-  return _stock[static_cast<std::size_t>(ing)].load(std::memory_order_acquire);
+  return _stock[ing].load(std::memory_order_acquire);
 }
 
-void Kitchen::Fridge::consume(Ingredient ing, uint8_t amount)
+void Kitchen::Fridge::consume(size_t ing, uint8_t amount)
 {
-  auto &val = _stock[static_cast<std::size_t>(ing)];
+  auto &val = _stock[ing];
   uint8_t current = val.load(std::memory_order_acquire);
   if (current < amount)
     throw std::runtime_error("Not enough stock to consume.");
@@ -44,63 +43,7 @@ void Kitchen::Fridge::consume(Ingredient ing, uint8_t amount)
     ;  // Retry till sucess
 }
 
-void Kitchen::Fridge::refill(Ingredient ing, uint8_t amount)
+void Kitchen::Fridge::refill(size_t ing, uint8_t amount)
 {
-  _stock[static_cast<std::size_t>(ing)].store(
-    amount, std::memory_order_release);
+  _stock[ing].store(amount, std::memory_order_release);
 }
-
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
-
-const Kitchen::RecipeBook Kitchen::_recipes = {
-  {Pizza::Margarita,
-   [] {
-     std::array<Fridge::Ingredient, MAX_INGREDIENTS> arr{};
-     arr[0] = Fridge::Ingredient::Dough;
-     arr[1] = Fridge::Ingredient::Tomato;
-     arr[2] = Fridge::Ingredient::Gruyere;
-     for (size_t i = 3; i < MAX_INGREDIENTS; ++i)
-       arr[i] = Fridge::Ingredient::COUNT;
-     return arr;
-   }()},
-
-  {Pizza::Regina,
-   [] {
-     std::array<Fridge::Ingredient, MAX_INGREDIENTS> arr{};
-     arr[0] = Fridge::Ingredient::Dough;
-     arr[1] = Fridge::Ingredient::Tomato;
-     arr[2] = Fridge::Ingredient::Gruyere;
-     arr[3] = Fridge::Ingredient::Ham;
-     arr[4] = Fridge::Ingredient::Mushrooms;
-     for (size_t i = 5; i < MAX_INGREDIENTS; ++i)
-       arr[i] = Fridge::Ingredient::COUNT;
-     return arr;
-   }()},
-
-  {Pizza::Americana,
-   [] {
-     std::array<Fridge::Ingredient, MAX_INGREDIENTS> arr{};
-     arr[0] = Fridge::Ingredient::Dough;
-     arr[1] = Fridge::Ingredient::Tomato;
-     arr[2] = Fridge::Ingredient::Gruyere;
-     arr[3] = Fridge::Ingredient::Steak;
-     for (size_t i = 4; i < MAX_INGREDIENTS; ++i)
-       arr[i] = Fridge::Ingredient::COUNT;
-     return arr;
-   }()},
-
-  {Pizza::Fantasia,
-   [] {
-     std::array<Fridge::Ingredient, MAX_INGREDIENTS> arr{};
-     arr[0] = Fridge::Ingredient::Dough;
-     arr[1] = Fridge::Ingredient::Tomato;
-     arr[2] = Fridge::Ingredient::Eggplant;
-     arr[3] = Fridge::Ingredient::GoatCheese;
-     arr[4] = Fridge::Ingredient::ChiefLove;
-     for (size_t i = 5; i < MAX_INGREDIENTS; ++i)
-       arr[i] = Fridge::Ingredient::COUNT;
-     return arr;
-   }()},
-};
