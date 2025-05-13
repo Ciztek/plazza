9fdcc1224350bb651e16ac74a0da29b2fb3340a1
Ciznia <gabriel.hosquet@epitech.eu>
2025-05-13 19:25:00 +0200

    Encapsulate global variable in a class

---
+++ include/Config.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <filesystem>
+
+#include "Data.hpp"
+#include "ErrorOr.hpp"
+#include "json-parser/JSONValue.hpp"
+
+#define EXIT_TEK 84
+
+#define CONFIG_FILE Config::File::instance()
+
+namespace Config {
+
+  class File {
+  public:
+    static auto instance() -> File &;
+
+    auto init() -> MaybeError;
+
+    [[nodiscard]] auto getIngredientsIds() const -> const Data::Ids &;
+
+    [[nodiscard]] auto getRecipesIds() const -> const Data::Ids &;
+
+    [[nodiscard]] auto getRecipesByIds() const -> const Data::RecipeBook &;
+
+    File(const File &) = delete;
+    auto operator=(const File &) -> File & = delete;
+    File(File &&) = delete;
+    auto operator=(File &&) -> File & = delete;
+
+  private:
+    File() = default;
+    ~File() = default;
+
+    auto
+    parse_recipe(const std::string &key, JSON::JSONValue &value) -> MaybeError;
+    auto parse(const std::filesystem::path &path) -> MaybeError;
+
+    Data::Ids _ingredientsIds;
+    Data::Ids _recipesIds;
+    Data::RecipeBook _recipesByIds;
+  };
+}  // namespace Config
