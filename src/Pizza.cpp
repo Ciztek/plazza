2fd291070d6f8442504639a84f297b4452cdbab1
Ciznia <gabriel.hosquet@epitech.eu>
2025-05-14 11:53:58 +0200

    Reduce the number of information source

--- src/Pizza.cpp
+++ src/Pizza.cpp
@@ -1,6 +1,9 @@
-#include "Pizza.hpp"
+
 #include <array>
 #include <cstdint>
+#include <cstring>
+
+#include "Pizza.hpp"
 
 using namespace Plazza;
 
@@ -10,49 +13,58 @@ namespace {
     return (1U << bits) - 1U;
   }
 
-  enum BitsOrder : std::uint8_t {
-    STAT_ORDER,
-    LEFT_ORDER,
-    SIZE_ORDER,
-    TYPE_ORDER,
-    ORDER_COUNT
+  struct FieldEntry {
+    const char *name;
+    uint8_t bits;
+    uint8_t shift;
   };
 
-  enum BitsSize : std::uint8_t {
-    STAT_BITS = 2,
-    LEFT_BITS = 3,
-    SIZE_BITS = 3,
-    TYPE_BITS = 8,
-  };
+  constexpr auto Fields = []() {
+    constexpr std::array<std::pair<const char *, uint8_t>, 4> fieldsInfo = {{
+      {"STAT", 2},
+      {"SIZE", 3},
+      {"LEFT", 3},
+      {"TYPE", 8},
+    }};
 
-  constexpr std::array<uint8_t, ORDER_COUNT> BitsSizes =
-    {STAT_BITS, LEFT_BITS, SIZE_BITS, TYPE_BITS};
-
-  constexpr auto computeShift(BitsOrder order) -> uint8_t
-  {
+    std::array<FieldEntry, fieldsInfo.size()> fields{};
     uint8_t shift = 0;
-    for (size_t i = 0; i < order; ++i)
-      shift += BitsSizes[i];
-    return shift;
+    for (size_t i = 0; i < fieldsInfo.size(); ++i) {
+      fields[i] = {
+        .name = fieldsInfo[i].first,
+        .bits = fieldsInfo[i].second,
+        .shift = shift};
+      shift += fieldsInfo[i].second;
+    }
+    return fields;
+  }();
+
+  constexpr auto findField(const char *name) -> FieldEntry
+  {
+    for (auto Field: Fields)
+      if (strcmp(Field.name, name) == 0)
+        return Field;
+    throw "Unknown field name";
   }
 
-  constexpr uint8_t STAT_SHIFT = computeShift(STAT_ORDER);
-  constexpr uint8_t LEFT_SHIFT = computeShift(LEFT_ORDER);
-  constexpr uint8_t SIZE_SHIFT = computeShift(SIZE_ORDER);
-  constexpr uint8_t TYPE_SHIFT = computeShift(TYPE_ORDER);
+  constexpr auto getMask(const char *name) -> uint16_t
+  {
+    auto field = findField(name);
+    return FIELD_MASK(field.bits) << field.shift;
+  }
 
-  enum Mask : std::uint16_t {
-    STAT_MASK = FIELD_MASK(STAT_BITS) << STAT_SHIFT,
-    LEFT_MASK = FIELD_MASK(LEFT_BITS) << LEFT_SHIFT,
-    SIZE_MASK = FIELD_MASK(SIZE_BITS) << SIZE_SHIFT,
-    TYPE_MASK = FIELD_MASK(TYPE_BITS) << TYPE_SHIFT
-  };
-}  // anonymous namespace
+  constexpr auto encodeField(uint16_t value, const char *name) -> uint16_t
+  {
+    auto field = findField(name);
+    return (value & FIELD_MASK(field.bits)) << field.shift;
+  }
 
-#define ENCODE_FIELD(value, FIELD)                                             \
-  (((value) & FIELD_MASK(FIELD##_BITS)) << FIELD##_SHIFT)
-#define DECODE_FIELD(value, FIELD)                                             \
-  (((value) & FIELD##_MASK) >> FIELD##_SHIFT)
+  constexpr auto decodeField(uint16_t value, const char *name) -> uint16_t
+  {
+    auto field = findField(name);
+    return (value >> field.shift) & FIELD_MASK(field.bits);
+  }
+}  // namespace
 
 Pizza::Pizza(uint8_t type, Size size, State state)
 {
@@ -61,8 +73,8 @@ Pizza::Pizza(uint8_t type, Size size, State state)
 
 auto Pizza::set(uint8_t type, Size size, State state) -> Pizza &
 {
-  uint16_t value = ENCODE_FIELD(type, TYPE) | ENCODE_FIELD(size, SIZE)
-    | ENCODE_FIELD(0, LEFT) | ENCODE_FIELD(state, STAT);
+  uint16_t value = encodeField(type, "TYPE") | encodeField(size, "SIZE")
+    | encodeField(0, "LEFT") | encodeField(state, "STAT");
 
   data.store(value, std::memory_order_release);
   return *this;
@@ -71,8 +83,8 @@ auto Pizza::set(uint8_t type, Size size, State state) -> Pizza &
 auto Pizza::setState(State newState) -> Pizza &
 {
   uint16_t current = raw();
-  uint16_t updated = (current & ~STAT_MASK)
-    | ENCODE_FIELD(static_cast<uint8_t>(newState), STAT);
+  uint16_t updated = (current & ~getMask("STAT"))
+    | encodeField(newState, "STAT");
 
   data.store(updated, std::memory_order_release);
   return *this;
@@ -91,15 +103,15 @@ auto Pizza::raw() const -> uint16_t
 
 auto Pizza::getType() const -> uint8_t
 {
-  return static_cast<uint8_t>(DECODE_FIELD(raw(), TYPE));
+  return static_cast<uint8_t>(decodeField(raw(), "TYPE"));
 }
 
 auto Pizza::getSize() const -> Pizza::Size
 {
-  return static_cast<Pizza::Size>(DECODE_FIELD(raw(), SIZE));
+  return static_cast<Pizza::Size>(decodeField(raw(), "SIZE"));
 }
 
 auto Pizza::getState() const -> Pizza::State
 {
-  return static_cast<State>(DECODE_FIELD(raw(), STAT));
+  return static_cast<State>(decodeField(raw(), "STAT"));
 }
