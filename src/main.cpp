c84bebe2813bda002edecdf607f408b39de1f969
Ciznia <gabriel.hosquet@epitech.eu>
2025-05-15 18:46:34 +0200

    Add the repl

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <cstdlib>
 
-int main(void)
+#include "ErrorOr.hpp"
+#include "Repl.hpp"
+
+namespace {
+
+  auto wrappedMain() -> MaybeError
+  {
+    Plazza::Repl repl;
+
+    TRY(repl.run());
+    return Nil{};
+  }
+}  // namespace
+
+auto main() -> int
 {
+  if (wrappedMain().is_error())
+    return 84;
   return EXIT_SUCCESS;
 }
