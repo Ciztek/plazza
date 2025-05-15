8c12f0b8b6b142f03a1075ab025041a2c138653d
Ciznia <gabriel.hosquet@epitech.eu>
2025-05-15 18:46:34 +0200

    Add the repl

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 
 #include "Config.hpp"
 #include "ErrorOr.hpp"
+#include "Repl.hpp"
 
 namespace {
 
   auto wrappedMain(int argc, char *argv[]) -> MaybeError
   {
+    Plazza::Repl repl;
+
     TRY(CONFIG_FILE.init());
     TRY(CONFIG_ARGS.init(argc, argv));
+    TRY(repl.run());
     return Nil{};
   }
 }  // namespace
